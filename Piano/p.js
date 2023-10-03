3b5690598d0425ae74bc1aecb161267746b8604b
AnujBhadoria30 <144831521+AnujBhadoria30@users.noreply.github.com>
2023-10-03 15:52:06 +0530

    updated piano in Volume

--- Piano/p.js
+++ Piano/p.js
@@ -1,73 +1,59 @@
-// let btn = document.getElementById("btn");
-// let music_a = document.getElementsByClassName("music")[0];
-// let music_d = document.getElementsByClassName("music")[1];
 let toggle = document.getElementById("spn_1");
-// let buttonclick=document.getElementsByClassName("key yup")[0];
-let div =document.querySelectorAll(".yup div")
-// console.log(div);
-
-
+let volume=document.querySelector(".volume")
+let div = document.querySelectorAll(".yup div");
 flag = true;
-div.forEach(h=>{
-  h.classList.add("hidden")
-})
-
+div.forEach((h) => {
+  h.classList.add("hidden");
+});
 function Event() {
   if (flag == true) {
-    console.log("inside1st")
+    // console.log("inside1st");
     btn.setAttribute("class", "move");
     toggle.setAttribute("class", "spn");
-    div.forEach(h=>{
-      h.classList.remove("hidden")
-      
-    })
-    
+    div.forEach((h) => {
+      h.classList.remove("hidden");
+    });
     flag = false;
   } else {
-    console.log("inside2st")
+    console.log("inside2st");
     toggle.removeAttribute("class", "spn");
     btn.removeAttribute("class", "move");
     flag = true;
-    div.forEach(h=>{
-      h.classList.add("hidden")
-      
-    })
-    
-    // div.forEach(h=>{
-    //   h.classList.remove("hidden")
-      
-    // })
+    div.forEach((h) => {
+      h.classList.add("hidden");
+    });
   }
-  
 }
-
-// buttonclick.addEventListener('onclick',(e)=>{
-//   console.log(e)
-//   music_a.play();
-// })
-// function fun()
-// {
-//   music_a.play();
-// }
-// let gana = new Audio();//by class add music
 let key = document.querySelectorAll(".yup");
-let gana =document.getElementById("music");
-function setmusic(data)
-{
-  gana.src=`tunes/${data}.wav`
-  // if(data=='A')
-  // {
-  //   gana.src ="tunes/A.wav"
-  // }
-  console.log(gana);
-  
+let gana = document.getElementById("music");
+function setmusic(data) {
+  gana.src = `tunes/${data}.wav`;
+  // console.log(gana);   
   gana.play();
 }
-// console.log(key);
-key.forEach(value =>{
-  console.log(value);
-  value.addEventListener("click",(e)=>{
-    console.log(value.dataset.key);
-    setmusic(value.dataset.key);
-  })
+key.forEach((values) => {
+  values.addEventListener("click", (e) => {
+    setmusic(values.dataset.key)
+    
+  });
+});
+volume.addEventListener("click",(e)=>{
+  // console.log(e.target.value)
+  gana.volume=e.target.value;
 })
+// const arrkeys =['A','S','D','F','H','U','O','L','P',';','W','E','T','G','Y','J','K']
+const arrkeys =[];
+key.forEach((value)=>{
+  arrkeys.push(value.dataset.key)
+
+})
+
+
+document.addEventListener("keydown",(e)=>
+  {
+    // console.log(e.key);
+    if(arrkeys.includes((e.key).toUpperCase()))
+    {setmusic(e.key)}
+    
+  })
+
